00d47fd5300204f548264752040aaaf9fdcedb4f
Juan Carlos <jcarlosmc1994@gmail.com>
2017-07-09 17:32:24 -0600

    Nueva Imagen de dominio

--- Proyecto_II_Lenguajes/Proyecto_II_Library/Domain/Imagen.cs
+++ Proyecto_II_Lenguajes/Proyecto_II_Library/Domain/Imagen.cs
@@ -9,12 +9,10 @@ namespace Proyecto_II_Library.Domain
     public class Imagen
     {
         private int idImagen;
-        //private Image imagen;
-
-        public Imagen()
-        {
-
-        }
+        private string nombre;
+        private int size;
+        private string contentType;
+        private byte[] data;
 
         public int IdImagen
         {
@@ -27,6 +25,72 @@ namespace Proyecto_II_Library.Domain
             {
                 idImagen = value;
             }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+
+            set
+            {
+                size = value;
+            }
+        }
+
+        public string ContentType
+        {
+            get
+            {
+                return contentType;
+            }
+
+            set
+            {
+                contentType = value;
+            }
+        }
+
+        public byte[] Data
+        {
+            get
+            {
+                return data;
+            }
+
+            set
+            {
+                data = value;
+            }
+        }
+
+        public Imagen()
+        {
+
+        }
+
+        public Imagen(int idImagen, string nombre, int size, string contentType, byte[] data)
+        {
+            this.idImagen = idImagen;
+            this.nombre = nombre;
+            this.size = size;
+            this.contentType = contentType;
+            this.data = data;
         }
     }
 }
